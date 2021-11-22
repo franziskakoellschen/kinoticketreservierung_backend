6be85a8e4a79da74f415e9301630f914933708b8
niklasweidenfeller <77716996+niklasweidenfeller@users.noreply.github.com>
2021-11-22 12:41:00 +0100

    Test CORS (#9)
    * Test Cors
    
    * fix tests on PR
    
    * test
    
    * Update README.md
    
    * Update java_ci.yml
    
    Co-authored-by: Niklas Weidenfeller <niklas.weidenfeller@sap.com>

---
+++ src/test/java/com/kinoticket/backend/TestContollerTests.java
@@ -0,0 +1,40 @@
+package com.kinoticket.backend;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.containsString;
+
+@SpringBootTest
+public class TestContollerTests {
+    
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
+    MockMvc mvc;
+
+    @BeforeEach
+    void before() {
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+	@Test
+	void getTest() throws Exception{
+		this.mvc.perform(get("/testRequest"))
+		.andExpect(status().isOk())
+		.andExpect(
+			content().string(
+				containsString("Hello from Backend")
+			)
+		);
+	}
+
+}

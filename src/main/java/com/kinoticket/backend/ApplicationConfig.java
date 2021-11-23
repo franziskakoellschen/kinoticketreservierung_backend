2e9538993b0ac608c73537c01bc651a778a6b96f
niklasweidenfeller <77716996+niklasweidenfeller@users.noreply.github.com>
2021-11-23 10:15:15 +0100

    Test MoviesController, Test DataSource, refactor (#11)
    * Unit tests for movies controller and ApplicationConfig
    
    * organize imports
    
    * refactor CORS logic to ApplicationConfig class
    
    * fix test
    
    Co-authored-by: Niklas Weidenfeller <niklas.weidenfeller@sap.com>

--- src/main/java/com/kinoticket/backend/ApplicationConfig.java
+++ src/main/java/com/kinoticket/backend/ApplicationConfig.java
@@ -11,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @PropertySource("classpath:application.properties")
@@ -18,6 +20,11 @@ public class ApplicationConfig {
 
     Logger logger = LoggerFactory.getLogger(ApplicationConfig.class);
     
+	/**
+	 * Connects to a PostgreSQL Database given the DATABASE_URL is
+	 * set as an environment variable.
+	 * 		@return The PostgreSQL Database as DataSource
+	 */
     @Bean
     public DataSource dataSource() throws URISyntaxException {
 
@@ -39,4 +46,25 @@ public class ApplicationConfig {
             return null;
         }
     }
+    
+	/**
+	 * Allows requests from 'Cross-Origin Resource Sharing' URLs.
+	 * 		@return WebMvcConfigurer with allowed CORS origins.
+	 */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {		
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(
+					"http://localhost:3000",
+					"http://kinoticket-frontend-dev.herokuapp.com",
+					"http://kinoticket-frontend-prod.herokuapp.com",
+					"https://localhost:3000",
+					"https://kinoticket-frontend-dev.herokuapp.com",
+					"https://kinoticket-frontend-prod.herokuapp.com"
+				);
+			}
+		};
+	}
 }

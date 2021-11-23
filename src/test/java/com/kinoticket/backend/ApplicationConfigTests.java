8923aa04b9d32251263ce1e29bb9006f4bd5f791
niklasweidenfeller <niklas.weidenfeller@gmx.de>
2021-11-23 09:46:25 +0100

    refactor CORS logic to ApplicationConfig class

--- src/test/java/com/kinoticket/backend/ApplicationConfigTests.java
+++ src/test/java/com/kinoticket/backend/ApplicationConfigTests.java
@@ -3,6 +3,8 @@ package com.kinoticket.backend;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.net.URISyntaxException;
 
@@ -11,10 +13,18 @@ import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.junitpioneer.jupiter.ClearEnvironmentVariable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+@SpringBootTest
 public class ApplicationConfigTests {
 
-    
+	@Autowired
+	WebApplicationContext webApplicationContext;
+
     @Test
     void testDataSource() throws URISyntaxException {
         assertDoesNotThrow(new Executable() {
@@ -38,4 +48,34 @@ public class ApplicationConfigTests {
             }
         });
     }
+
+	@Test
+	void corsOriginsAllowed() throws Exception {
+		MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+
+		// test forbidden origin
+		mockMvc.perform(
+			options("/test-cors")
+			.header("Access-Control-Request-Method", "GET")
+			.header("Origin", "http://www.someurl.com")
+		).andExpect(status().isForbidden());
+
+		// test allowed origins
+		String[] allowedOrigins = {
+			"http://localhost:3000",
+			"http://kinoticket-frontend-dev.herokuapp.com",
+			"http://kinoticket-frontend-prod.herokuapp.com",
+			"https://localhost:3000",
+			"https://kinoticket-frontend-dev.herokuapp.com",
+			"https://kinoticket-frontend-prod.herokuapp.com"
+		};
+
+		for(String origin : allowedOrigins) {
+			mockMvc.perform(
+				options("/test-cors")
+				.header("Access-Control-Request-Method", "GET")
+				.header("Origin", origin)
+			).andExpect(status().isOk());
+		}
+	}
 }

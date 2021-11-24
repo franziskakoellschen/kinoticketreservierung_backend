8744caa3b971802db06aaf3eb17cd80cf5fccd61
Niklas Weidenfeller <niklas.weidenfeller@sap.com>
2021-11-24 10:48:32 +0100

    Draft MoviesController

--- src/main/java/com/kinoticket/backend/rest/MoviesController.java
+++ src/main/java/com/kinoticket/backend/rest/MoviesController.java
@@ -1,28 +1,39 @@
 package com.kinoticket.backend.rest;
 
+import com.kinoticket.backend.model.FimShow;
 import com.kinoticket.backend.model.Movie;
-import com.kinoticket.backend.repositories.MovieRepository;
+import com.kinoticket.backend.service.MoviesService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MoviesController {
+
     @Autowired
-    MovieRepository repository;
-
-    public MoviesController() {}
-
-    @GetMapping("/movies")
-    public Iterable<Movie> getAll() {
-        return repository.findAll();
-    }
+    MoviesService service;
 
     @PostMapping("/movies")
     public Movie postMovie(@RequestBody Movie movie) {
-        return repository.save(movie);
+        return service.putMovie(movie);
+    }
+
+    @GetMapping("/movies")
+    public Iterable<Movie> getMovies() {
+        return service.getMovies();
+    }
+
+    @GetMapping("/movies/{id}")
+    public Movie getMovie(@PathVariable int id) {
+        return service.getMovie(id);
+    }
+
+    @GetMapping("/movies/{id}/filmShows")
+    public Iterable<FimShow> getFilmShows(@PathVariable int movieId) {
+        return service.getFilmShows(movieId);
     }
 }

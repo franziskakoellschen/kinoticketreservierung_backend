f16540cbce223e8251f21635487938b7e341abd1
Alina Brockmann <brockmannalina@web.de>
2021-11-28 21:58:58 +0100

    Test for SeatsPK

--- src/test/java/com/kinoticket/backend/model/SeatTest.java
+++ src/test/java/com/kinoticket/backend/model/SeatTest.java
@@ -2,7 +2,7 @@ package com.kinoticket.backend.model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SeatTest {
 
@@ -39,4 +39,55 @@ public class SeatTest {
         assertEquals(reserved, seat.isReserved());
         assertEquals(priceCategory, seat.getPriceCategory());
     }
+
+    @Test
+    void seatPKEqualsTest() {
+
+        SeatPK seatPK = new SeatPK();
+        seatPK.row = 1;
+        seatPK.seatNumber = 10;
+        seatPK.bookingplan = 5;
+
+        SeatPK seatPK2 = new SeatPK();
+        seatPK2.row = 1;
+        seatPK2.seatNumber = 10;
+        seatPK2.bookingplan = 5;
+
+        SeatPK seatPK3 = new SeatPK();
+        seatPK3.row = 10;
+        seatPK3.seatNumber = 10;
+        seatPK3.bookingplan = 5;
+
+        SeatPK seatPK4 = new SeatPK();
+        seatPK4.row = 10;
+        seatPK4.seatNumber = 10;
+        seatPK4.bookingplan = 6;
+
+        SeatPK seatPK5 = new SeatPK();
+        seatPK5.row = 10;
+        seatPK5.seatNumber = 11;
+        seatPK5.bookingplan = 6;
+
+        assertEquals(seatPK, seatPK2);
+        assertNotEquals(seatPK, seatPK3);
+        assertNotEquals(seatPK, seatPK4);
+        assertNotEquals(seatPK, seatPK5);
+
+        BookingPlan bookingPlan = new BookingPlan();
+
+        assertNotEquals(seatPK, bookingPlan);
+    }
+
+    @Test
+    void seatPKHashcodeTest() {
+
+        SeatPK seatPK = new SeatPK();
+        seatPK.row = 1;
+        seatPK.seatNumber = 10;
+        seatPK.bookingplan = 5;
+
+        assertEquals(686469, seatPK.hashCode());
+
+    }
+
 }

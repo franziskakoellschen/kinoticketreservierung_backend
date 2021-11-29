b16dc0b33ddea9cf052cdd0b73c4ff427fa2bd78
Niklas Weidenfeller <niklas.weidenfeller@sap.com>
2021-11-29 22:52:34 +0100

    clean imports

--- src/test/java/com/kinoticket/backend/service/MovieServiceTest.java
+++ src/test/java/com/kinoticket/backend/service/MovieServiceTest.java
@@ -15,8 +15,6 @@ import com.kinoticket.backend.model.Movie;
 import com.kinoticket.backend.repositories.FilmShowRepository;
 import com.kinoticket.backend.repositories.MovieRepository;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;

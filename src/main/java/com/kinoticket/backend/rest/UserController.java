1159bf286be98a420e879e0971c358e90284f713
GregorMuenker <gregormuenker@googlemail.com>
2021-11-30 16:19:16 +0100

    wip

--- src/main/java/com/kinoticket/backend/rest/UserController.java
+++ src/main/java/com/kinoticket/backend/rest/UserController.java
@@ -1,11 +1,18 @@
 package com.kinoticket.backend.rest;
 
+import com.kinoticket.backend.Exceptions.EntityNotFound;
+import com.kinoticket.backend.Exceptions.MissingParameterException;
 import com.kinoticket.backend.model.BankDetails;
 import com.kinoticket.backend.model.User;
 import com.kinoticket.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -14,18 +21,45 @@ public class UserController {
     UserService service;
 
     @PostMapping()
-    public void postUser(@RequestBody User user) {
-        service.createUser(user);
+    public ResponseEntity<User> postUser(@RequestBody User user) {
+        ResponseEntity<User> responseEntity = null;
+        User sentUser = null;
+        try {
+            service.createUser(user);
+            responseEntity = new ResponseEntity<User>(HttpStatus.OK);
+        } catch ( MissingParameterException mp){
+            log.error("Missing Parameter: " + mp.getMessage());
+            responseEntity = new ResponseEntity<User>(sentUser,HttpStatus.BAD_REQUEST);
+        }
+        return responseEntity;
     }
 
     @GetMapping("/{id}")
-    public User getUser(@PathVariable long id) {
-        return service.getUser(id);
+    public ResponseEntity<User> getUser(@PathVariable long id) {
+        ResponseEntity<User> responseEntity = null;
+        User sentUser = null;
+        try {
+            sentUser = service.getUser(id);
+            responseEntity = new ResponseEntity<User>(sentUser,HttpStatus.OK);
+        } catch ( EntityNotFound nf){
+            log.error("Invalid Parameter: " + nf.getMessage());
+            responseEntity = new ResponseEntity<User>(sentUser,HttpStatus.BAD_REQUEST);
+        }
+        return responseEntity;
     }
 
     @GetMapping("/getid/{email}")
-    public long getUserId(@PathVariable String email) {
-        return service.getUserId(email);
+    public ResponseEntity<User> getUser(@PathVariable String email) {
+        ResponseEntity<User> responseEntity = null;
+        User sentUser = null;
+        try {
+            sentUser = service.getUser(email);
+            responseEntity = new ResponseEntity<User>(sentUser,HttpStatus.OK);
+        } catch ( EntityNotFound nf){
+            log.error("Invalid Parameter: " + nf.getMessage());
+            responseEntity = new ResponseEntity<User>(sentUser,HttpStatus.BAD_REQUEST);
+        }
+        return responseEntity;
     }
 
     @PatchMapping("/{id}")
--- src/main/java/com/kinoticket/backend/service/UserService.java
+++ src/main/java/com/kinoticket/backend/service/UserService.java
@@ -1,5 +1,7 @@
 package com.kinoticket.backend.service;
 
+import com.kinoticket.backend.Exceptions.EntityNotFound;
+import com.kinoticket.backend.Exceptions.MissingParameterException;
 import com.kinoticket.backend.model.BankDetails;
 import com.kinoticket.backend.model.User;
 import com.kinoticket.backend.repositories.AddressRepository;
@@ -19,16 +21,24 @@ public class UserService {
     @Autowired
     private BankDetailRepository bankDetailRepository;
 
-    public void createUser(User user){
+    public void createUser(User user) throws MissingParameterException {
 
     }
 
-    public User getUser(long id){
+    public User getUser(long id) throws EntityNotFound {
+        User requestedUser = userRepository.findById(id);
+        if(requestedUser == null) {
+            throw new EntityNotFound("Can't find User by Id" + id);
+        }
         return null;
     }
 
-    public long getUserId (String email){
-        return 0;
+    public User getUser(String email) throws EntityNotFound {
+        User requestedUser = userRepository.findByEmail(email);
+        if(requestedUser == null) {
+            throw new EntityNotFound("Can't find User by Email" + email);
+        }
+        return requestedUser;
     }
 
     public void updateUser(long id){

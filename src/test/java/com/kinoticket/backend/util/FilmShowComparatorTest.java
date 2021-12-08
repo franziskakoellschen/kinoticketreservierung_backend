61f7775e71cc5575c9c4a74990d8cab583d3e7e1
Niklas Weidenfeller <niklas.weidenfeller@sap.com>
2021-12-08 18:07:45 +0100

    fix null pointer issue

--- src/test/java/com/kinoticket/backend/util/FilmShowComparatorTest.java
+++ src/test/java/com/kinoticket/backend/util/FilmShowComparatorTest.java
@@ -26,18 +26,24 @@ public class FilmShowComparatorTest {
         show2.setTime(Time.valueOf("19:00:00"));
         FilmShow show3 = new FilmShow();
         show3.setId(3);
-        show3.setDate(Date.valueOf("2021-12-07"));
-        show3.setTime(Time.valueOf("18:00:00"));
+        show3.setDate(null);
+        show3.setTime(null);
+        FilmShow show4 = new FilmShow();
+        show4.setId(4);
+        show4.setDate(Date.valueOf("2021-12-07"));
+        show4.setTime(Time.valueOf("18:00:00"));
 
         filmShows.add(show1);
         filmShows.add(show2);
         filmShows.add(show3);
+        filmShows.add(show4);
 
         filmShows.sort(new FilmShowComparator());
 
-        assertEquals(3, filmShows.get(0).getId());
+        assertEquals(4, filmShows.get(0).getId());
         assertEquals(2, filmShows.get(1).getId());
         assertEquals(1, filmShows.get(2).getId());
+        assertEquals(3, filmShows.get(3).getId());
 
     }
 }

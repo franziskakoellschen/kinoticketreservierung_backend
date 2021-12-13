40b9df42d7ebe2cacb83b5584653bce496652342
niklasweidenfeller <niklas.weidenfeller@gmx.de>
2021-12-13 19:48:39 +0100

    ability to clean tables

--- testdata/dataUploader.py
+++ testdata/dataUploader.py
@@ -1,29 +1,72 @@
 import csv
 import psycopg2
 
-print("Database host (z.B. 'localhost'):")
-url = input()
-print("Database password:")
-pwd = input()
+def getConnection():
+    print("Database host (z.B. 'localhost'):")
+    url = input()
+    print("Database password:")
+    pwd = input()
 
-conn = psycopg2.connect("host=" + url + " dbname=postgres user=postgres password=" + pwd)
+    conn = psycopg2.connect("host=" + url + " dbname=postgres user=postgres password=" + pwd)
 
-cur = conn.cursor()
+    return conn
 
-data = {
-    # number of '%s' reflects the number of columns in a table
-    {"cinema_hall.csv", "INSERT INTO cinema_hall VALUES (%s, %s, %s)"},
-    {"seats.csv", "INSERT INTO seats VALUES (%s, %s, %s, %s, %s)"},
-    {"movie.csv", "INSERT INTO movies VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"},
-    {"filmshow.csv", "INSERT INTO filmshow VALUES (%s, %s, %s, %s, %s)"},
-    {"filmshow_seat.csv", "INSERT INTO filmshow_seat VALUES (%s, %s, %s)"}
-}
+def uploadCSVs(conn):
+    cur = conn.cursor()
 
-for csvFile, sql in data:
-    with open('./database_tables/' + csvFile, 'r') as f:
-        reader = csv.reader(f)
-        next(reader) # Skip the header row.
-        for row in reader:
-            cur.execute(sql, row)
+    data = {
+        # number of '%s' reflects the number of columns in a table
+        {"cinema_hall.csv", "INSERT INTO cinema_hall VALUES (%s, %s, %s)"},
+        {"seats.csv", "INSERT INTO seats VALUES (%s, %s, %s, %s, %s)"},
+        {"movie.csv", "INSERT INTO movies VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"},
+        {"filmshow.csv", "INSERT INTO filmshow VALUES (%s, %s, %s, %s, %s)"},
+        {"filmshow_seat.csv", "INSERT INTO filmshow_seat VALUES (%s, %s, %s)"}
+    }
 
-conn.commit()
+    for csvFile, sql in data:
+        with open('./database_tables/' + csvFile, 'r') as f:
+            reader = csv.reader(f)
+            next(reader) # Skip the header row.
+            for row in reader:
+                cur.execute(sql, row)
+
+    conn.commit()
+
+    print("Success")
+    
+def cleanTables(conn):
+    cur = conn.cursor()
+    tables = {
+        "cinema_hall",
+        "seats",
+        "movies",
+        "filmshow",
+        "filmshow_seat"
+    }
+
+    for table in tables:
+        cur.execute("DELETE CASCADE from %s", table)
+    
+    conn.commit()
+    
+    print("Success")
+
+
+if __name__ == "__main__":
+    conn = getConnection()
+    
+    while (True):
+        print("1: Clean Tables")
+        print("2: Upload CSVs")
+        print("3: Exit")
+        
+        selection = input()
+        
+        if selection == 1:
+            cleanTables(conn)
+        
+        if selection == 2:
+            uploadCSVs(conn)
+            
+        if selection == 3:
+            exit()

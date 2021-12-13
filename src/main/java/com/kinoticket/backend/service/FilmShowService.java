d4a2ff8db445a55e08cee3aa9c6590bf4a538f12
Niklas Weidenfeller <niklas.weidenfeller@sap.com>
2021-12-13 09:11:54 +0100

    posting filmShows

--- src/main/java/com/kinoticket/backend/service/FilmShowService.java
+++ src/main/java/com/kinoticket/backend/service/FilmShowService.java
@@ -7,11 +7,14 @@ import com.kinoticket.backend.repositories.FilmShowSeatRepository;
 import com.kinoticket.backend.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
 
 import java.sql.Date;
 import java.sql.Time;
 
 import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 public class FilmShowService {
@@ -28,7 +31,7 @@ public class FilmShowService {
     @Autowired
     MovieRepository movieRepository;
 
-    public void postFilmShow(Date date, Time time, long movieId, long cinemaHallId){
+    public FilmShow postFilmShow(Date date, Time time, long movieId, long cinemaHallId){
         Optional<CinemaHall> cinemaHallR = cinemaHallRepository.findById(cinemaHallId);
         Optional<Movie> movieR = movieRepository.findById(movieId);
         if (cinemaHallR.isPresent() && movieR.isPresent()) {
@@ -37,14 +40,29 @@ public class FilmShowService {
             FilmShow filmShow = new FilmShow(date, time, movie, cinemaHall, null);
             filmShow.setCinemaHall(cinemaHall);
             filmShowRepository.save(filmShow);
+
+            // adding seats
             for (Seat s : cinemaHall.getSeats()) {
                 FilmShowSeat filmShowSeat = new FilmShowSeat(s, filmShow, false);
                 filmShowSeatRepository.save(filmShowSeat);
             }
+
+            filmShow.setFilmShow_seats(
+                filmShowSeatRepository.findByFilmShow_id(
+                    filmShow.getId()
+            ));
+            return filmShow;
         }
+        return null;
     }
 
     public FilmShow findById(long filmShowId) {
         return filmShowRepository.findById(filmShowId).get();
     }
+
+
+    public Iterable<FilmShow> getAllFilmShows() {
+        return filmShowRepository.findAll();
+    }
+
 }

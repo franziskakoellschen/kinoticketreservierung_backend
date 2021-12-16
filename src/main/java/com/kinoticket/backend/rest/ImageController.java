518d675df7af89b43c96f4856d83f37f5c26b536
Niklas Weidenfeller <niklas.weidenfeller@sap.com>
2021-12-16 18:46:21 +0100

    organize imports

--- src/main/java/com/kinoticket/backend/rest/ImageController.java
+++ src/main/java/com/kinoticket/backend/rest/ImageController.java
@@ -1,18 +1,12 @@
 package com.kinoticket.backend.rest;
 
-import com.kinoticket.backend.model.Image;
 import com.kinoticket.backend.repositories.ImageDbRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
-
-import javax.annotation.Resource;
-import java.io.IOException;
-import java.io.InputStream;
 
 
 @RestController

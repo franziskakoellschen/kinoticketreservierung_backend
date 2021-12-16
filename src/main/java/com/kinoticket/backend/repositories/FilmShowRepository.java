7b425f9f04fd5c04e4a0a892899e9ab2e1d115ec
Alina Brockmann <brockmannalina@web.de>
2021-12-16 19:19:25 +0100

    jpql instead of native sql

--- src/main/java/com/kinoticket/backend/repositories/FilmShowRepository.java
+++ src/main/java/com/kinoticket/backend/repositories/FilmShowRepository.java
@@ -11,11 +11,9 @@ import java.util.List;
 
 public interface FilmShowRepository extends CrudRepository<FilmShow, Long> {
 
-    @Query(value = "SELECT * FROM FilmShow f WHERE date(f.date) > ?1 OR(date(f.date) = ?1 AND f.time >= ?2)",
-            nativeQuery = true)
+    @Query(value = "SELECT f FROM FilmShow f WHERE date(f.date) > ?1 OR(date(f.date) = ?1 AND f.time >= ?2)")
     List<FilmShow> findFutureFilmShows(Date currentDate, Time currentTime);
 
-    @Query(value = "SELECT * FROM FilmShow f WHERE f.movie_id = ?1 AND (date(f.date) > ?2 OR(date(f.date) = ?2 AND f.time >= ?3))",
-            nativeQuery = true)
+    @Query(value = "SELECT f FROM FilmShow f WHERE movie_id = ?1 AND (date(f.date) > ?2 OR(date(f.date) = ?2 AND f.time >= ?3))")
     List<FilmShow> findFutureFilmShowsByMovie(long id, Date currentDate, Time currentTime);
 }

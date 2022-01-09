1b9e13243551cfe53c9514802f7961a54c402aa4
Niklas Weidenfeller <niklas.weidenfeller@sap.com>
2022-01-09 10:16:38 +0100

    fix build

--- src/main/java/com/kinoticket/backend/rest/UserController.java
+++ src/main/java/com/kinoticket/backend/rest/UserController.java
@@ -14,6 +14,7 @@ import com.kinoticket.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +28,7 @@ import java.util.Optional;
 
 @RequestMapping("/user")
 @RestController
-
+@PreAuthorize("hasRole('USER')")
 public class UserController {
     @Autowired
     UserRepository userRepository;
@@ -52,7 +53,10 @@ public class UserController {
 
         Optional<User> user = userRepository.findById(requestedUser.getId());
 
-
+        if (user.isPresent()) {
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
     @PostMapping()
